b09fbe28b57a81a1612e750c7672e432dd2a7b91
Furkan TOSUN <74304973+Scorpowl@users.noreply.github.com>
2023-08-12 03:51:06 +0300

    Update linear_regresion.py

---
+++ linear_regresion.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split, cross_val_score
+
+pd.set_option("display.float_format", lambda x: '%.2f' % x)
+
+
+
+
+
+
+
