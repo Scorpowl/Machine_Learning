edf2c21c40bc9597f1ec38c1b205e5e549ef9f26
Furkan TOSUN <74304973+Scorpowl@users.noreply.github.com>
2023-08-13 15:12:04 +0300

    Update Gradient_descent.py

--- Gradient_descent.py
+++ Gradient_descent.py
@@ -13,6 +13,7 @@ df = pd.read_csv("datasets/advertising.csv")
 
 #MSE FUNCTION
 #Y = bağımlı değişken sayısı
+
 def cost_function(Y, b, w, X):
     m = len(Y)
     sse = 0
@@ -22,6 +23,38 @@ def cost_function(Y, b, w, X):
         sse += (y_tah - y_ger) **2 
         mse = sse / m
         return mse
+    
+def update_weight(Y, b, w, X, learning_rate):
+    m = len(Y)
+    b_deriv_sum = 0
+    w_deriv_sum = 0
+
+    for i in range(0,m):
+        y_hat = b + w * x[i]
+        y_ger = Y[i]
+
+        b_deriv_sum += (y_hat - y_ger)
+        w_deriv_sum += (y_hat - y_ger) * X[i]
+
+    new_b = b - (learning_rate * 1 / m * b_deriv_sum)
+    new_w = w - (learning_rate * 1 / m * w_deriv_sum)
+    return new_b, new_w
+
+def train(Y, initial_b, initial_w, X, learning_rate, num_iters):
+    print("Starting gradient descent at b = {0}, w = {1}, mse = {2}".format(initial_b, initial_w, cost_function(Y, initial_b, initial_w, X)))
+
+    b = initial_b
+    w = initial_w
+    cost_history = []
+    for i in range(num_iters):
+        b, w = update_weight(Y, b, w, X, learning_rate)
+        mse = cost_function(Y, b, w, X)
+        cost_history.append(mse)
+        if i % 100 == 0:
+            print("iter = {:d}    b = {:.2f}  mse= {:.4f}".format(i,b,w,mse))
+    print("After {0} iterations b = {1}, w = {2}, mse = {3}".format(num_iters,b,w, cost_function(Y,b,w,X)))
+    return cost_history, b ,w 
+
     
 
 
