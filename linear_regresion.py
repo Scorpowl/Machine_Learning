c069bddd2380fdfcd3402a4147655140e0c37b33
Furkan TOSUN <74304973+Scorpowl@users.noreply.github.com>
2023-08-13 13:35:56 +0300

    Update linear_regresion.py

--- linear_regresion.py
+++ linear_regresion.py
@@ -5,6 +5,7 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split, cross_val_score
+from skimpy import skim
 
 pd.set_option("display.float_format", lambda x: '%.2f' % x)
 
@@ -14,38 +15,51 @@ pd.set_option("display.float_format", lambda x: '%.2f' % x)
 
 df = pd.read_csv("datasets/advertising.csv")
 
-X = df[["TV"]]
-y = df[["sales"]] 
+# X = df[["TV"]]
+# y = df[["sales"]] 
 
-#MODEL
+# #MODEL
 
-reg_model = LinearRegression().fit(X,y)
+# reg_model = LinearRegression().fit(X,y)
 
-#b
-reg_model.intercept_[0]
+# #b
+# reg_model.intercept_[0]
 
-#w
-reg_model.coef_[0][0]
+# #w
+# reg_model.coef_[0][0]
 
-#TAHMİN
+# #TAHMİN
 
-#150 BİRİMLİK TV HARCAMASINDA NE KADAR SATIŞ OLMASI BEKLENİR?
+# #150 BİRİMLİK TV HARCAMASINDA NE KADAR SATIŞ OLMASI BEKLENİR?
 
-reg_model.intercept_[0] + reg_model.coef_[0][0] * 150
+# reg_model.intercept_[0] + reg_model.coef_[0][0] * 150
 
-g = sns.regplot(x=X, y=y, scatter_kws={'color': 'b', 's': 9},
-                ci=False, color="r")
+# g = sns.regplot(x=X, y=y, scatter_kws={'color': 'b', 's': 9},
+#                 ci=False, color="r")
 
-g.set_title(f"Model Denklemi: Sales = {round(reg_model.intercept_[0], 2)} + TV*{round(reg_model.coef_[0][0], 2)}")
-g.set_ylabel("Satış Sayısı")
-g.set_xlabel("TV Harcamaları")
-# plt.xlim(-10, 310)
-# plt.ylim(bottom=0)
-# plt.show()
+# g.set_title(f"Model Denklemi: Sales = {round(reg_model.intercept_[0], 2)} + TV*{round(reg_model.coef_[0][0], 2)}")
+# g.set_ylabel("Satış Sayısı")
+# g.set_xlabel("TV Harcamaları")
+# # plt.xlim(-10, 310)
+# # plt.ylim(bottom=0)
+# # plt.show()
 
 
-y_pred = reg_model.predict(X)
-hata = mean_squared_error(y , y_pred)
-print(hata)
+# #MSE 10.5
+# y_pred = reg_model.predict(X)
+# hata = mean_squared_error(y , y_pred)
+
+# #RMSE 3.24
+# hata2 = np.sqrt(hata)
+
+# #MAE 2.54
+# hata3 = mean_absolute_error(y , y_pred)
+
+# print(hata,hata2,hata3)
+
+# #R-KARE
+# reg_model.score(X, y)
+
+skim(df)
 
 
