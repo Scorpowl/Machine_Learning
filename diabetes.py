f555f860c6163665d94aeb4c8490781da1225d2f
Furkan TOSUN <74304973+Scorpowl@users.noreply.github.com>
2023-08-14 18:02:08 +0300

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -118,4 +118,55 @@ roc_auc_score(y, y_prob)
 # Model Validation: Holdout
 ###################
 
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.20, random_state=17)
+
+log_model = LogisticRegression().fit(X_train, y_train)
+
+y_pred = log_model.predict(X_test)
+y_prob = log_model.predict_proba(X_test)[:, 1]
+
+# print(classification_report(y_test, y_pred))
+
+# Accuracy: 0.78
+# Precision: 0.74
+# Recall: 0.58
+# F1-score: 0.65
+
+# Accuracy: 0.77
+# Precision: 0.79
+# Recall: 0.53
+# F1-score: 0.63
+
+# RocCurveDisplay(log_model, X_test, y_test)
+# plt.title('ROC Curve')
+# plt.plot([0, 1], [0, 1], 'r--')
+# plt.show()
+
+# AUC
+roc_auc_score(y_test, y_prob)
+
+#############################
+# Model Validation: 10-Fold Cross Validation
+#############################
+
+y = df["Outcome"]
+X = df.drop(["Outcome"], axis=1)
+
+log_model = LogisticRegression().fit(X, y)
+
+cv_results = cross_validate(log_model,
+                            X, y,
+                            cv=5,
+                            scoring=["accuracy", "precision", "recall", "f1", "roc_auc"])
+
+
+
+
+
+
+
+
+
 
