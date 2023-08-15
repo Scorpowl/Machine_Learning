c95e3f695aca69d1b59e2f0ca51a654f7c69f61f
Furkan TOSUN <74304973+Scorpowl@users.noreply.github.com>
2023-08-15 14:22:35 +0300

    Update knn.py

---
+++ knn.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.model_selection import GridSearchCV, cross_validate
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("datasets/diabetes.csv")
+
+y = df["Outcome"]
+X = df.drop(["Outcome"], axis=1)
+
+X_scaled = StandardScaler().fit_transform(X)
+
+X = pd.DataFrame(X_scaled, columns=X.columns)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
