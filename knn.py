b62885ee281e941e832fece8ef22068a71c2f47c
Furkan TOSUN <74304973+Scorpowl@users.noreply.github.com>
2023-08-18 13:49:28 +0300

    Update knn.py

--- knn.py
+++ knn.py
@@ -4,6 +4,17 @@ from sklearn.model_selection import GridSearchCV, cross_validate
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 
+################################################
+# KNN
+################################################
+
+# 1. Exploratory Data Analysis
+# 2. Data Preprocessing & Feature Engineering
+# 3. Modeling & Prediction
+# 4. Model Evaluation
+# 5. Hyperparameter Optimization
+# 6. Final Model
+
 df = pd.read_csv("datasets/diabetes.csv")
 
 y = df["Outcome"]
