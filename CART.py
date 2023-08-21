8df4cbc26114c48de95f9f7552f643c56dd64a5d
Furkan TOSUN <74304973+Scorpowl@users.noreply.github.com>
2023-08-21 17:47:32 +0300

    Update CART.py

--- CART.py
+++ CART.py
@@ -158,5 +158,23 @@ mean_test_score = np.mean(test_score, axis=1)
 # plt.legend(loc='best')
 # plt.show()
 
+def val_curve_params(model, X, y, param_name, param_range, scoring="roc_auc", cv=10):
+    train_score, test_score = validation_curve(
+        model, X=X, y=y, param_name=param_name, param_range=param_range, scoring=scoring, cv=cv)
 
+    mean_train_score = np.mean(train_score, axis=1)
+    mean_test_score = np.mean(test_score, axis=1)
+
+    plt.plot(param_range, mean_train_score,
+             label="Training Score", color='b')
+
+    plt.plot(param_range, mean_test_score,
+             label="Validation Score", color='g')
+
+    plt.title(f"Validation Curve for {type(model).__name__}")
+    plt.xlabel(f"Number of {param_name}")
+    plt.ylabel(f"{scoring}")
+    plt.tight_layout()
+    plt.legend(loc='best')
+    plt.show(block=True)
 
