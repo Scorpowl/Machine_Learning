584fb3cd9ab3bce4e02bb3549efb80f1862e3839
Furkan TOSUN <74304973+Scorpowl@users.noreply.github.com>
2023-08-30 17:36:52 +0300

    Create Advanced_trees.py

---
+++ Advanced_trees.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import warnings
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
+from sklearn.model_selection import GridSearchCV, cross_validate, RandomizedSearchCV, validation_curve
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
